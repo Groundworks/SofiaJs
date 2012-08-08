b58e8cbe16565b2233530c3ad4966aaa6c38232b
Jacob Groundwater <groundwater@gmail.com>
2012-08-08 00:48:51 -0700

    Make Dynamic

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,33 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
 
 object Application extends Controller {
+  
+  val data = Map(
+    "#main" -> "This data has been loaded dynamically via Javascript</a>",
+    "#nav1" -> "<ul><li>One</li><li>Two</li></ul>"
+  )
   
   def index = Action {
     Redirect("/assets/example.html")
   }
   
+  // Serve Content via JSON API
+  def content = Action { request =>
+    request.body.asJson.map { json =>
+      (json \ "location").asOpt[String].map { location =>
+        
+        // Insert Dynamic Content
+        val data2 = data + ("#sub1" -> ("This content is intended for the page: "+ location))
+        
+        Ok( Json.toJson(data2) )
+      } .getOrElse {
+        BadRequest("JSON Request Must Include Location Parameter")
+      }
+    }.getOrElse {
+      BadRequest("Expecting Json data")
+    }
+  }
 }
--- public/current.js
+++ public/current.js
@@ -1,11 +1,36 @@
-$.ajax({
-  url: "/assets/default.json",
-  dataType: "json",
-  success:function(data, textStatus, jqXHR){
-    for(key in data){
-      value = data[key];
-      $(key).html(value);
+function log(message){
+  $("#console").append($("<p>").text(message));
+}
+
+$(function(){
+  
+  var pagekey = window.location.href;
+  var request = {
+    location: pagekey,
+    version : "0.0.0-alpha",
+  };
+  
+  log("Loading content for page: " + pagekey);
+  
+  $.ajax({
+    url:  "/content",
+    type: "POST",
+    dataType: "json",
+    data: JSON.stringify(request),
+    contentType: "application/json; charset=utf-8",
+    success:function(data, textStatus, jqXHR){
+      log("Json Response Containing Content Received")
+      
+      for(key in data){
+        log("Inserting Content into Element: "+key)
+        value = data[key];
+        $(key).html(value);
+      }
+      log("Content Loaded");
+    },
+    error:function(jqXHR, textStatus, errorThrown){
+      log("There was an Error: " + textStatus);
     }
-    $("#console").html("Loading content for page: " + window.location.href);
-  }
+  });
+  
 });
