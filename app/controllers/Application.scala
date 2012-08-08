859a5d491efba5e6fc9b17dee51f7b82a555a832
Jacob Groundwater <groundwater@gmail.com>
2012-08-08 01:04:25 -0700

    Migrate Example to Controller

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,6 +4,8 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 
+import java.net.URL
+
 object Application extends Controller {
   
   val data = Map(
@@ -12,7 +14,11 @@ object Application extends Controller {
   )
   
   def index = Action {
-    Redirect("/assets/example.html")
+    Redirect("/example")
+  }
+  
+  def page(page:String) = Action {
+    Ok( views.html.example() )
   }
   
   // Serve Content via JSON API
@@ -20,10 +26,14 @@ object Application extends Controller {
     request.body.asJson.map { json =>
       (json \ "location").asOpt[String].map { location =>
         
+        val url = new URL(location)
+        val path = url.getPath()
+        
         // Insert Dynamic Content
-        val data2 = data + ("#sub1" -> ("This content is intended for the page: "+ location))
+        val data2 = data + ("#sub1" -> ("This content is intended for the page: "+ path))
         
         Ok( Json.toJson(data2) )
+        
       } .getOrElse {
         BadRequest("JSON Request Must Include Location Parameter")
       }
