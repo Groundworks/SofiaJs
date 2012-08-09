cec28b54fa1de994b26f2d14c054360e0f1ae44a
Jacob Groundwater <groundwater@gmail.com>
2012-08-08 19:52:10 -0700

    Fix Database Implementation Issue

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,9 +3,10 @@
 # --- !Ups
  
 CREATE TABLE page (
+    id bigint(20) NOT NULL AUTO_INCREMENT,
     pagekey varchar(255) NOT NULL,
     content TEXT NOT NULL,
-    PRIMARY KEY (pagekey)
+    PRIMARY KEY (id)
 );
  
 # --- !Downs
